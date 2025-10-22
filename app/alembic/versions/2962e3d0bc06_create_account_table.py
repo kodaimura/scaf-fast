ac5e09e0af0e0f1990263d963fcae84e69e2293b
kodaimura <kodaimura123@gmail.com>
2025-10-23 00:49:53 +0900

    update account migration

--- app/alembic/versions/2962e3d0bc06_create_account_table.py
+++ app/alembic/versions/2962e3d0bc06_create_account_table.py
@@ -1,10 +1,11 @@
 """create account table
 
 Revision ID: 2962e3d0bc06
-Revises: 
+Revises:
 Create Date: 2025-10-22 15:25:42.205566
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,7 +13,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '2962e3d0bc06'
+revision: str = "2962e3d0bc06"
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -20,9 +21,30 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     """Upgrade schema."""
-    pass
+    op.create_table(
+        "account",
+        sa.Column("id", sa.BigInteger, primary_key=True, autoincrement=True),
+        sa.Column("email", sa.Text, nullable=False, unique=True),
+        sa.Column("password_hash", sa.Text, nullable=False),
+        sa.Column("first_name", sa.String(100), nullable=False),
+        sa.Column("last_name", sa.String(100), nullable=False),
+        sa.Column("deleted_at", sa.DateTime, nullable=True),
+        sa.Column(
+            "created_at",
+            sa.DateTime(timezone=True),
+            server_default=sa.func.now(),
+            nullable=False,
+        ),
+        sa.Column(
+            "updated_at",
+            sa.DateTime(timezone=True),
+            server_default=sa.func.now(),
+            onupdate=sa.func.now(),
+            nullable=False,
+        ),
+    )
 
 
 def downgrade() -> None:
     """Downgrade schema."""
-    pass
+    op.drop_table("account")
